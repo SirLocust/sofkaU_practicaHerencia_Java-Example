224f4f79367dbfbe57ec4c6a9ef0e8beb858f60f
sirlocust <jua56yu@hotmail.com>
2021-09-02 10:49:51 -0500

    creado base del proyecto

---
+++ src/main/java/com/sirlocust/java_exercice/App.java
@@ -0,0 +1,13 @@
+package com.sirlocust.java_exercice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
